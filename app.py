b33fd5733c4373658e6f05c652de1af9efe1428c
leaherb <leah@erbinweb.com>
2021-07-05 10:49:32 -0700

    add pytest

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ def hello():
 if __name__ == "__main__":
     ## stream logs to app.log file
     logging.basicConfig(filename='app.log',level=logging.DEBUG)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080)
 
